a58d389fe7626ef9cb5ca7f604a178d85d18cc87
HannaMin8 <anyamin1984@gmail.com>
2022-11-02 22:17:50 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Weather App</div>;
-}
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
 
-export default App;
+        <footer>
+          This project was coded by Hanna Minovska and is{" "}
+          <a
+            href="https://github.com/HannaMin8/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
+}
